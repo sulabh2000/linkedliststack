4ec9b3c1e5c8d2d26be7b744053c4ebc346169ee
sulabh2000 <bansalsulabh13@gmail.com>
2019-03-05 15:31:29 +0530

    init commit

---
+++ src/main/Stack.java
@@ -0,0 +1,65 @@
+package main;
+
+public class Stack {
+    private static class Node
+    {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next)
+        {
+            this.data=data;
+            this.next=next;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+    private Node top = null;
+    private int size = 0;
+    public void push(int data)
+    {
+        Node newNode = new Node(data, this.top);
+        this.top = newNode;
+        this.size++;
+    }
+    public int pop()
+    {
+        int response = -1;
+        if(this.top!=null)
+        {
+            response = this.top.getData();
+            this.top = this.top.getNext();
+            size--;
+        }
+        else
+        {
+            System.out.println("Stack is Empty");
+        }
+        return response;
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        System.out.println(stack);
+        for (int i = 0; i < 5; i++) {
+            stack.push(i+1);
+        }
+        stack.pop();
+        System.out.println(stack);
+    }
+
+}
