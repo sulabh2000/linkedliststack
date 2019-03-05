5ef7704649fec7778352e5e9a80fac9703ad2380
sulabh2000 <bansalsulabh13@gmail.com>
2019-03-05 15:40:03 +0530

    add toString method

--- src/main/Stack.java
+++ src/main/Stack.java
@@ -61,5 +61,24 @@ public class Stack {
         stack.pop();
         System.out.println(stack);
     }
+    @Override
+    public String toString()
+    {
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+        Node temp = this.top;
+        while (temp != null)
+        {
+
+            result.append(temp.getData());
+            if(temp.getNext()!=null)
+            {
+                result.append(" --> ");
+            }
+            temp=temp.getNext();
+        }
+        result.append("]");
+        return result.toString();
+    }
 
 }
